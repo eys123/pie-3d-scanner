152a67075b2c501239eaa98e33f5c29ac9bbb741
dfriishecht <dfriishecht@olin.edu>
2023-09-21 14:48:37 -0400

    Update serial_communication.py

--- serial_communication.py
+++ serial_communication.py
@@ -1,15 +1,34 @@
 import serial
 import time
 
-arduino = serial.Serial(port='COM4', baudrate=115200, timeout=0.1)
+arduino = serial.Serial(port='COM6', baudrate=9600, timeout=0.1)
+arduino_port = "COM6"
+fileName = "analog-data.csv"
+samples = 10
+print_labels = False
 
-def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
-    time.sleep(0.05)
-    data = arduino.readline()
-    return data
+print("Connected to Arduino port" + arduino_port)
+file = open(fileName, "a")
+print("created file")
+line = 0
 
-while True:
-    num = input("Enter a number: ")
-    value = write_read(num)
-    print(value)
+while line <= samples:
+    if print_labels:
+        if line == 0:
+            print("Printing Column Headers")
+        else:
+            print("Line" + str(line) + ": writing...")
+    getData = str(arduino.readline())
+    data = getData[0:]
+    print(data)
+
+#def write_read(x):
+  #  arduino.write(bytes(x, 'utf-8'))
+  #  time.sleep(0.05)
+  #  data = arduino.readline()
+   # return data
+
+#while True:
+ #   num = input("Enter a number: ")
+  #  value = write_read(num)
+  #  print(value)
