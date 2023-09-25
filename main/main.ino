103d94a20dc2aebe5ac721c84e9c274d8e7e56a5
eys123 <esun@olin.edu>
2023-09-25 15:47:18 -0400

    Add button to start scan

--- main/main.ino
+++ main/main.ino
@@ -2,41 +2,47 @@
 
 Servo vertServo;
 Servo horServo;
-int vPos = 0;
-int hPos = 0;
-int sensorPin = A0;
+const int vertPin = 9;
+const int horPin = 8;
+int vPos = 55;
+int hPos = 50;
+const int sensorPin = A0;
 int sensorVal = 0;
 int sensorCoor[] = {0, 0};
+const int buttonPin = 13;
+int buttonState = 0;
+int prevButtonState = 0;
 
 void setup() {
   Serial.begin(9600);
-  vertServo.attach(9);
-  horServo.attach(8);
+  vertServo.attach(vertPin);
+  horServo.attach(horPin);
   vertServo.write(vPos);
   horServo.write(hPos);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  // if button switches low -> high activate scan
-  // while button is high turn off the sense different state thing
-  // once button goes low turn it back on again
-
-  // horizontal scan
-  for (hPos = 0; hPos <= 180; hPos += 5) {
-    horServo.write(hPos);
-    for (vPos = 0; vPos <= 120; vPos +=5) {
-      vertServo.write(vPos);
-      sensorVal = analogRead(sensorPin);
-      sensorCoor[0] = hPos;
-      sensorCoor[1] = vPos;
-      printSensorInfo(sensorVal, sensorCoor);
-      delay(150);
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH & prevButtonState == LOW) {
+    for (hPos = 50; hPos <= 130; hPos +=2) {
+      horServo.write(hPos);
+      for (vPos = 55; vPos <= 150; vPos +=2) {
+        vertServo.write(vPos);
+        sensorVal = analogRead(sensorPin);
+        sensorCoor[0] = hPos;
+        sensorCoor[1] = vPos;
+        printSensorInfo(sensorVal, sensorCoor);
+        delay(75);
+      }
+      delay(75);
     }
-    delay(150);
+    vertServo.write(55);
+    horServo.write(50);
+    delay(500);
   }
-  vertServo.write(0);
-  horServo.write(0);
-  delay(500);
+  prevButtonState = buttonState;
+
 }
 
 void printSensorInfo(int sensorVal, int sensorCoor[]) {
@@ -47,3 +53,4 @@ void printSensorInfo(int sensorVal, int sensorCoor[]) {
   Serial.print(sensorCoor[1]);
   Serial.println("]");
 }
+
