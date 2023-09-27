c43281e0096b96b92bbb840b843526985abb6a07
dfriishecht <dfriishecht@olin.edu>
2023-09-27 19:46:44 -0400

    Add function docstring

--- spherical_to_cart.py
+++ spherical_to_cart.py
@@ -6,6 +6,19 @@ import random
 
 
 def convert_spherical_to_cartesian(phi, theta, r):
+    """
+    Converts spherical coordinates to cartesian coordinates.
+
+    Args:
+        phi - an integer representing horizontal rotation.
+        theta - an integer representing vertical rotation.
+        r - a float representing distance from the sensor.
+    
+    Returns:
+        (x, y, z) - Cartesian coordinate equivalent of the
+        spherical input.
+
+    """
     theta = np.deg2rad(theta)
     phi = np.deg2rad(phi)
     x = np.sin(theta)*np.cos(phi)*r
