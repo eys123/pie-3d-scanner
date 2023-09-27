388da6b096127c0932fc54163d39ce00de7ac784
eys123 <esun@olin.edu>
2023-09-27 15:25:10 -0400

    Make vertical mvmt less taxing, add documentation

--- main/main.ino
+++ main/main.ino
@@ -1,14 +1,19 @@
 # include <Servo.h>
-
 Servo vertServo;
 Servo horServo;
 const int vertPin = 9;
 const int horPin = 8;
-int vPos = 55;
-int hPos = 50;
+int vStart = 54;
+int hStart = 50;
+int vEnd = 150;
+int hEnd = 120;
+int vPos = vStart;
+int hPos = hStart;
+
 const int sensorPin = A0;
 int sensorVal = 0;
 int sensorCoor[] = {0, 0};
+
 const int buttonPin = 13;
 int buttonState = 0;
 int prevButtonState = 0;
@@ -23,29 +28,29 @@ void setup() {
 }
 
 void loop() {
+  // check if button is pressed
   buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH & prevButtonState == LOW) {
-    for (hPos = 50; hPos <= 130; hPos +=2) {
+    // execute horizontal movement
+    for (hPos = hStart; hPos <= hEnd; hPos +=1) {
       horServo.write(hPos);
-      for (vPos = 55; vPos <= 150; vPos +=2) {
-        vertServo.write(vPos);
-        sensorVal = analogRead(sensorPin);
-        sensorCoor[0] = hPos;
-        sensorCoor[1] = vPos;
-        printSensorInfo(sensorVal, sensorCoor);
-        delay(75);
-      }
+      vertMovement();
       delay(75);
     }
-    vertServo.write(55);
-    horServo.write(50);
-    delay(500);
+    vertServo.write(vStart);
+    horServo.write(hStart);
   }
   prevButtonState = buttonState;
-
 }
 
 void printSensorInfo(int sensorVal, int sensorCoor[]) {
+  /*
+    Prints sensor values and coordinates to Serial Monitor.
+    Args:
+      sensorVal: an int recording the output value of the distance sensor.
+      sensorCoor: an array containing 2 ints recording the horizontal servo angle
+      and the vertical servo angle.
+  */
   Serial.print(sensorVal);
   Serial.print(", [");
   Serial.print(sensorCoor[0]);
@@ -54,3 +59,29 @@ void printSensorInfo(int sensorVal, int sensorCoor[]) {
   Serial.println("]");
 }
 
+void vertMovement() {
+  /*
+    Vertical servo scanning movement.
+    Scans up on one scan, then scans downward on the next.
+  */
+  if (vPos >= vEnd) {
+    for (vPos = vEnd; vPos >= vStart; vPos -=1) {
+      vertServo.write(vPos);
+      sensorVal = analogRead(sensorPin);
+      sensorCoor[0] = hPos;
+      sensorCoor[1] = vPos;
+      printSensorInfo(sensorVal, sensorCoor);
+      delay(75);
+    }
+  }
+  else {
+    for (vPos = vStart; vPos <= vEnd; vPos +=1) {
+      vertServo.write(vPos);
+      sensorVal = analogRead(sensorPin);
+      sensorCoor[0] = hPos;
+      sensorCoor[1] = vPos;
+      printSensorInfo(sensorVal, sensorCoor);
+      delay(75);
+    }
+  }
+}
