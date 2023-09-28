e2f14ad7ce2b9c75e8a82a5c7d060ddc7dbf32d2
dfriishecht <dfriishecht@olin.edu>
2023-09-27 20:56:24 -0400

    Add function docstrings

--- serial_communication.py
+++ serial_communication.py
@@ -1,30 +1,32 @@
+"""
+Package for pulling serial data from Arduino.
+"""
 import serial
 import csv
 import datetime
 from time import sleep
 
 def read_and_store_serial_data(arduino_port, csv_filename, baud_rate=9600):
+    """
+    Reads data from the arduino serial port and stores it in a csv file
 
-        ser = serial.Serial(arduino_port, baud_rate)
+    Args:
+        arduino_port - string
+            The name of the arduino port to be accessed.
+        csv_filename - string
+            The desired name for the saved csv file.
+        baud_rate - int
+            The speed of communication between python and the arduino.
+    """
+    ser = serial.Serial(arduino_port, baud_rate)
         # Create or open the CSV file for writing
-        with open(csv_filename, 'w', newline='') as csvfile:
-            csv_writer = csv.writer(csvfile)
-            
-            # Write a header row to the CSV file
-
-            while True:
-                # Read data from the Arduino
-                serial_data = ser.readline().decode().strip()
-                
-                # Get the current timestamp
-                #timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                
-                # Print and write the data to the CSV file
-                #print(f"{timestamp}: {serial_data}")
-                print(serial_data)
-                csv_writer.writerow([serial_data])
-
-
+    with open(csv_filename, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        while True:
+            # Read data from the Arduino
+            serial_data = ser.readline().decode().strip()
+            print(serial_data)
+            csv_writer.writerow([serial_data])
 
 if __name__ == "__main__":
     arduino_port = 'COM9'  # Arduino's serial port
