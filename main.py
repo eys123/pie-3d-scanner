94da69401f48be26f88852f1514f0ec8dfcf4b83
dfriishecht <dfriishecht@olin.edu>
2023-09-27 20:19:16 -0400

    Rename b.py to main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-
+"""
+Main file for calling functions and graphing data.
+"""
 from callibration_convert import read_data, line_fit
 from spherical_to_cart import convert_spherical_to_cartesian
 import csv
@@ -7,19 +9,17 @@ import math
 import matplotlib.pyplot as plt
 
 
-#data = read_and_store_serial_data("COM8", "scan_data.csv")
-
+# Data for sensor callibration
 xdata = np.array([20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80,
          85, 90, 95, 100, 105, 115, 125])
 ydata = np.array([473, 423, 375, 334, 295, 265, 249, 219, 200, 184, 173, 165, 153,
          148, 140, 132, 124, 120, 112, 108])
 
+# Generating the curve of best fit for callibration
 dist_convert = line_fit(ydata, xdata)
-print(dist_convert)
-
+# Pulling distance and angle readings from a saved csv file
 dist_list, angle_list = read_data("scan_data.csv", dist_convert)
-print(dist_list)
-coords = []
+# Callibrating coordinates and converting them from spherical to cartesian
 xlist = []
 ylist = []
 zlist = []
@@ -32,17 +32,11 @@ for i in range(len(dist_list)):
       xlist.append(x)
       ylist.append(y)
       zlist.append(z)
-     coords.append([x,y,z])
 
-print(coords)
+# Plotting the point cloud data
 fig=plt.figure()
 ax = plt.axes(projection='3d')
-#for i in range(len(coords)):
 ax.scatter(xlist, ylist,zlist, s=2)
-
-#ax.set_xlim(-200,200)
-#ax.set_ylim(-200,200)
-#ax.set_zlim(0,200)
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
